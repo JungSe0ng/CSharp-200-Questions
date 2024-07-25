139f5ee9b7a28bd16d841b2aa13dc1c50f368708
JungSe0ng <jungsam000@gmail.com>
2024-07-25 23:58:00 +0900

    0725_Upload

---
+++ CSharp-200-Questions/A005_string/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace A005_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello";
+            string b = "h";
+
+            b = b + "ello";
+            Console.WriteLine(a == b);
+            Console.WriteLine("b = " + b);
+
+            int x = 10;
+            string c = b + '!' + " " + x;
+            Console.WriteLine("c = " + c);
+        }
+    }
+}
---
+++ CSharp-200-Questions/A007_ConsoleWriteLine/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace A007_ConsoleWriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            char c = 'A';
+            decimal d = 1.234m;     // m은 decimal 형의 접미사
+            double e = 1.23456789;      // f는 float 형의 접미사
+            float f = 1.23456789f;
+            int i = 1234;
+            string s = "Hello";
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+            
+        }
+    }
+}
